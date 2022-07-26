d5c3b96275cbbb81882a8cb9e8be8f38603ad01f
徐瑞超 <xrcdev@163.com>
2022-07-26 22:23:50 +0800

    test net

---
+++ Sample/GrpcGreeterClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+using GrpcService1;
+
+namespace GrpcGreeterClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("begin!");
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://10.10.13.137:5056", new GrpcChannelOptions() { Credentials = null });
+
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
